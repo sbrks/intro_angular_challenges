25ae6e9593ce84fc9f7f4217794929660a19e08c
SBRKS <sbrks3@gmail.com>
2016-07-03 12:25:43 -0700

    challenges 1 and 2

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -6,3 +6,40 @@ console.log('challenge #2 -- app.js loaded!');
 // function WelcomeController(){
 //   this.full_name = "YOUR NAME"
 // }
+
+var app = angular.module("appTwo", []);
+
+app.controller("WelcomeController", WelcomeController);
+app.controller("ClassController", ClassController);
+
+function WelcomeController() {
+  this.full_name = "Sam";
+  this.age = 28;
+  this.city = "Bridgewater";
+  this.state = "Massachusetts";
+  this.full_name.length = 3;
+}
+
+function shout(name) {
+  name.toUpperCase;
+
+}
+
+function ClassController () {
+  this.class_name = "Gaia";
+  this.enrolled_students = "18";
+  this.start_date = new Date(2016,04,02);
+  this.end_date = new Date(2016,06,22);
+
+
+  function daysRemaining (firstDate, secondDate) {
+  var oneDay = 24*60*60*1000; // hours * minutes * seconds * milliseconds
+  var firstDate = new Date(2016,04,02);
+  var secondDate = new Date(2016,06,22);
+
+  var diffDays = Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneDay));
+
+}
+
+}
+
